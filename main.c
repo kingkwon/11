d5fd39d2d5d17512b0e0618a4c838e671273e53a
kingkwon <55428422+kingkwon@users.noreply.github.com>
2020-11-11 01:21:25 +0900

    실습 01

--- main.c
+++ main.c
@@ -3,6 +3,23 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(void) {
+	
+	char *pc;
+	int *pi;
+	double *pd;
+	
+	pc = (char *)10000;
+	pi = (int *)10000;
+	pd = (double *)10000;
+	
+	printf("Before increasing : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	
+	printf("After increasing : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
 	return 0;
 }
